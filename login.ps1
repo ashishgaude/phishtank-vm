de1c1238ca8c4c42524c5004f37efd8af0eb97b0
Ashish Gaude <ashish@numinolabs.com>
2017-12-29 14:19:04 +0530

    added all variables with login script

--- login.ps1
+++ login.ps1
@@ -1,4 +1,21 @@
-$securePassword = ConvertTo-SecureString -String $args[1] -AsPlainText -Force
+$subscriptionId = "2b7e3cc8-a58b-416a-8609-66dba589f974"
+$resourceGroupName = "phishtank-vm-rg"
+$virtualMachineName = "phishtank-vm"
+$disk = "phishtankOSDisk"
+$nicName = "phishtank-vm_nic" 
+$publicIpName = "phishtank-vm_ip"
 
-$cred = New-Object System.Management.Automation.PSCredential ($args[0], $securePassword)
+$snapshotResourceGroup = "phishtank-snapshot-rg"
+$snapshotName = "phishtankvmsnapshot"
+$osDiskName = "phishtankOSDisk"
+$virtualNetworkName = 'phishtank-vmVNET'
+$virtualMachineSize = 'Standard_D8_v3'
+
+$username = "ashish@numinolabs.com"
+$securePassword = ConvertTo-SecureString -String "xxx-xxx" -AsPlainText -Force
+
+
+# login to Azure
+$cred = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
 Add-AzureRmAccount -Credential $cred
+Set-AzureRMContext -SubscriptionId $subscriptionId
