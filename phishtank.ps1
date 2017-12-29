3528ac8b6e99336d3393ec5e2934f36fb6e43e73
Ashish Gaude <ashish@numinolabs.com>
2017-12-29 14:56:03 +0530

    Added virtual machine part

--- phishtank.ps1
+++ phishtank.ps1
@@ -22,11 +22,28 @@ echo "Setting up the subscription to " $subscriptionId
 Set-AzureRMContext -SubscriptionId $subscriptionId
 
 # delete the currupted vm along with its resources
-echo "Removing Virtual Machine: " $virtualMachineName
 Remove-AzureRmVM -Name $virtualMachineName -ResourceGroupName $resourceGroupName -Force -Verbose
-echo "Deleting Disk:" $osDiskName
 Remove-AzureRmDisk -Name $osDiskName -ResourceGroupName $resourceGroupName -Force -Verbose
-echo "Deleting Network Interface: " $nicName
 Remove-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $resourceGroupName -Force -Verbose
-echo "Removing Public IP Address" $publicIpName
 Remove-AzureRmPublicIpAddress -Name $publicIpName -ResourceGroupName $resourceGroupName -Force -Verbose
+
+# Creating virtual machine
+echo "Fetching the Snapshot..."
+$snapshot = Get-AzureRmSnapshot -ResourceGroupName $snapshotResourceGroup -SnapshotName $snapshotName 
+$diskConfig = New-AzureRmDiskConfig -AccountType $storageType -Location $snapshot.Location -SourceResourceId $snapshot.Id -CreateOption Copy
+echo "Creating Disk..."
+$disk = New-AzureRmDisk -Disk $diskConfig -ResourceGroupName $resourceGroupName -DiskName $osDiskName
+echo "Creating Virtual Machine Object..."
+$VirtualMachine = New-AzureRmVMConfig -VMName $virtualMachineName -VMSize $virtualMachineSize
+echo "Creating OS Disk..."
+$VirtualMachine = Set-AzureRmVMOSDisk -VM $VirtualMachine -ManagedDiskId $disk.Id -CreateOption Attach -Windows
+echo "Creating public IP..."
+$publicIp = New-AzureRmPublicIpAddress -Name ($VirtualMachineName.ToLower() + '_ip') -ResourceGroupName $resourceGroupName -Location $snapshot.Location -AllocationMethod Dynamic
+echo "Getting Virtual Network..."
+$vnet = Get-AzureRmVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName
+echo "Creating Network Interface..."
+$nic = New-AzureRmNetworkInterface -Name ($VirtualMachineName.ToLower() + '_nic') -ResourceGroupName $resourceGroupName -Location $snapshot.Location -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $publicIp.Id
+echo "Attach NIC to VM object"
+$VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -Id $nic.Id
+echo "Creating VIRTUAL MACHINE..."
+New-AzureRmVM -VM $VirtualMachine -ResourceGroupName $resourceGroupName -Location $snapshot.Location
