1b7ab88a60430ad697e2b66e1c0d38c62db13b36
Ashish Gaude <ashish@numinolabs.com>
2017-12-29 14:34:47 +0530

    Added script to delete the currupted vm

--- phishtank.ps1
+++ phishtank.ps1
@@ -1,13 +1,12 @@
 $subscriptionId = "2b7e3cc8-a58b-416a-8609-66dba589f974"
 $resourceGroupName = "phishtank-vm-rg"
 $virtualMachineName = "phishtank-vm"
-$disk = "phishtankOSDisk"
+$osDiskName = "phishtankOSDisk"
 $nicName = "phishtank-vm_nic" 
 $publicIpName = "phishtank-vm_ip"
 
 $snapshotResourceGroup = "phishtank-snapshot-rg"
 $snapshotName = "phishtankvmsnapshot"
-$osDiskName = "phishtankOSDisk"
 $virtualNetworkName = 'phishtank-vmVNET'
 $virtualMachineSize = 'Standard_D8_v3'
 
@@ -21,3 +20,13 @@ echo "Login in to Azure:"
 Add-AzureRmAccount -Credential $cred
 echo "Setting up the subscription to " $subscriptionId
 Set-AzureRMContext -SubscriptionId $subscriptionId
+
+# delete the currupted vm along with its resources
+echo "Removing Virtual Machine: " $virtualMachineName
+Remove-AzureRmVM -Name $virtualMachineName -ResourceGroupName $resourceGroupName -Force -Verbose
+echo "Deleting Disk:" $osDiskName
+Remove-AzureRmDisk -Name $osDiskName -ResourceGroupName $resourceGroupName -Force -Verbose
+echo "Deleting Network Interface: " $nicName
+Remove-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $resourceGroupName -Force -Verbose
+echo "Removing Public IP Address" $publicIpName
+Remove-AzureRmPublicIpAddress -Name $publicIpName -ResourceGroupName $resourceGroupName -Force -Verbose
